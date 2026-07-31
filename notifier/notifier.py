4b2aa8bbd78c5e83fb17c449a5cc5683fdc0333c
AKASH KUMAR <54479803+akashkr119@users.noreply.github.com>
2026-07-31 08:37:03 +0530

    Add Notifier implementation

---
+++ notifier/notifier.py
@@ -0,0 +1,17 @@
+class Notifier:
+    """Basic notification service."""
+
+    def send_email(self, recipient, subject, body):
+        return {
+            'success': True,
+            'channel': 'email',
+            'recipient': recipient,
+            'subject': subject
+        }
+
+    def send_telegram(self, chat_id, message):
+        return {
+            'success': True,
+            'channel': 'telegram',
+            'chat_id': chat_id
+        }
