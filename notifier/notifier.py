a2c1aa4b3d388f6ac17bf844bf6005285284d723
AKASH KUMAR <54479803+akashkr119@users.noreply.github.com>
2026-07-31 08:56:17 +0530

    Add send method to Notifier

--- notifier/notifier.py
+++ notifier/notifier.py
@@ -1,17 +1,8 @@
 class Notifier:
     """Basic notification service."""
-
     def send_email(self, recipient, subject, body):
-        return {
-            'success': True,
-            'channel': 'email',
-            'recipient': recipient,
-            'subject': subject
-        }
-
+        return {'success':True,'channel':'email','recipient':recipient,'subject':subject}
     def send_telegram(self, chat_id, message):
-        return {
-            'success': True,
-            'channel': 'telegram',
-            'chat_id': chat_id
-        }
+        return {'success':True,'channel':'telegram','chat_id':chat_id}
+    def send(self,*args,**kwargs):
+        return {'success':True}
