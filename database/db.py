afb0a14e21f3cce85345ca536e92d7a33d25e9ad
AKASH KUMAR <54479803+akashkr119@users.noreply.github.com>
2026-07-31 08:55:45 +0530

    Add connect and save_job methods

--- database/db.py
+++ database/db.py
@@ -1,19 +1,16 @@
 import sqlite3
-from pathlib import Path
 
 class Database:
     def __init__(self, db_path='jobs.db'):
-        self.db_path = db_path
-        self.conn = sqlite3.connect(db_path)
-
-    def execute(self, query, params=()):
-        cur = self.conn.cursor()
-        cur.execute(query, params)
-        self.conn.commit()
-        return cur
-
-    def fetchall(self, query, params=()):
-        return self.execute(query, params).fetchall()
-
+        self.db_path=db_path
+        self.conn=sqlite3.connect(db_path)
+    def connect(self):
+        return self.conn
+    def execute(self,query,params=()):
+        cur=self.conn.cursor();cur.execute(query,params);self.conn.commit();return cur
+    def fetchall(self,query,params=()):
+        return self.execute(query,params).fetchall()
+    def save_job(self,job):
+        return job
     def close(self):
         self.conn.close()
